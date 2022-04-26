21ae26efbb878d35cc1fe86a1b786abc94aae5b8
Stephen Gruzin <stephengrzn@gmail.com>
2022-04-25 18:22:12 -0700

    Email sends to my gmail

--- homeServer.js
+++ homeServer.js
@@ -2,6 +2,7 @@ const http = require("http");
 const express = require('express');
 const PORT = 7400;
 const app = express();
+const { sendMail } = require("./mail.js");
 
 app.set('view engine', 'ejs');
 app.use(express.json(), express.static('public'), express.urlencoded({ extended: true }));
@@ -12,6 +13,7 @@ app.get('/', (req, res) => res.render('index'));
 
 app.post("/contact", (req, res) => {
     console.log(req.body);
+    sendMail(req.body.email, req.body.what);
     res.status(200).json({msg: "Success!"})
 });
 
